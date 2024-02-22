5c62931f9a40d58ff2233a80fa295d1e3c3b2209
vishalipar <vishalipar04@gmail.com>
2024-02-22 23:53:44 +0530

    Happy people data.

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+st.title('In Search for Happiness')
+
+option_x = st.selectbox('Select the data for the X-axis',
+                        ('GDP', 'Happiness', 'Generosity'))
+option_y = st.selectbox('Select the data for Y-axis',
+                        ('GDP', 'Happiness', 'Generosity'))
+
+df = pd.read_csv('happy.csv')
+
+
+match option_x:
+    case 'Happiness':
+        x_array = df['happiness']
+    case 'GDP':
+        x_array = df['gdp']
+    case 'Generosity':
+        x_array = df['generosity']
+
+match option_y:
+    case 'Happiness':
+        y_array = df['happiness']
+    case 'GDP':
+        y_array = df['gdp']
+    case 'Generosity':
+        y_array = df['generosity']
+
+
+st.subheader(f'{option_x} and {option_y}')
+
+figure1 = px.scatter(x=x_array, y=y_array,
+                     labels={'x': option_x, 'y': option_y})
+
+st.plotly_chart(figure1)
